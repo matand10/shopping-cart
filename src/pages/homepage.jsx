8c1c2beae925bd3830319c6cfbcff2c0dcdea365
Matan Damary <matandamary10@gmail.com>
2022-07-25 11:10:43 +0300

    test

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -5,7 +5,7 @@ export const Homepage = () => {
 
 
     return <section className="homepage main-layout">
-        <h1>Hello from homepage</h1>
+        <h1>Hello from Matan Matan Matan</h1>
     </section>
 
 }
